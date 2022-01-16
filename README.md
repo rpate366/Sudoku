c7a8d4e9c8500a1985d053f99c87d2cb7f807786
rpate366 <71097350+rpate366@users.noreply.github.com>
2022-01-15 19:10:34 -0500

    final version

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # Sudoku
 Personal Project. Sudoku game with auto solver.
-
 main.py is the main file that runs. 
-    Contains the drawing functions, runtime checks, and the solver.
-
-generateBoard.py
-    Contains the functions for check and initializing the board.
+    Contains the setup and the runtime checks.
+    Also responsible for solving the board and verifying the game is winable, both of which are done by using backtracking.
 
 My Socials:
     GitHub - rpate366
     Email - rpate366@uwo.ca
     LinkedIn - www.linkedin.com/in/rajpatel366
+    Instagram - _patel_raj
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from operator import add
 import pygame
 import random
 import sys
-from generateBoard import createBoard, validEntry
 
 if __name__ == "__main__":
 
@@ -16,6 +15,17 @@ if __name__ == "__main__":
     icon = pygame.image.load('icon_v.jpeg')
     pygame.display.set_icon(icon)
     pygame.display.set_caption("SUDOKU USING BACKTRACKING")
+    grid = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
 
     #declaring var to setup board
         #x and y are coords
@@ -26,13 +36,11 @@ if __name__ == "__main__":
         #val is the current value to be assigned
     val = 0
 
-    difficulty = "MED"
-
-    #initialize the grid depending on the difficulty
-    grid = createBoard(difficulty= difficulty)
+    difficulty = input("Enter a difficulty (EASY, MED, HARD): ")
 
     #system font
     font1 = pygame.font.SysFont("timesnewroman",40)
+
 
     #highlight selection
     def draw_highlight():
@@ -40,8 +48,9 @@ if __name__ == "__main__":
             pygame.draw.line(screen, (255, 0, 0), (x * unit - 3, (y + i) * unit), (x * unit + unit + 3, (y + i) * unit), 7)
             pygame.draw.line(screen, (255, 0, 0), ( (x + i) * unit, y * unit ), ((x + i) * unit, y * unit + unit), 7)  
 
+
     #draw grid
-    def draw():
+    def draw(grid):
         for row in range (9):
             for col in range (9):
                 if grid[row][col]!= 0:
@@ -62,7 +71,9 @@ if __name__ == "__main__":
             pygame.draw.line(screen, (0, 0, 0), (0, row * unit), (s_width, row * unit), thick)
             pygame.draw.line(screen, (0, 0, 0), (row * unit, 0), (row * unit, s_width), thick)     
 
+    #solves given grid starting from given coordinates (used in backtracking)
     def solve(grid, i, j):
+        global prep
         while grid[i][j]!= 0:
             if i<8:
                 i+= 1
@@ -80,13 +91,12 @@ if __name__ == "__main__":
                 global x, y
                 x = i
                 y = j
-
-                # reset graphics for advance
+                # reset graphics
                 screen.fill((255, 255, 255))
-                draw()
+                draw(grid)
                 draw_highlight()
                 pygame.display.update()
-
+                #pygame.time.delay(2)
                 if solve(grid, i, j)== 1:
                     return True
                 else:
@@ -94,11 +104,97 @@ if __name__ == "__main__":
 
                 # reset graphics for backtrack
                 screen.fill((255, 255, 255))
-                draw()
+                draw(grid)
                 draw_highlight()
-                pygame.display.update()   
+                pygame.display.update()
+                #pygame.time.delay(1)   
                 
         return False 
+
+
+    #checking validity of entry
+    def validEntry(grid, row, col, val):
+        #check row and col for recurrences
+        for it in range(9):
+            if grid[row][it]== val:
+                return False
+            if grid[it][col]== val:
+                return False
+        #check subgrid for recurrences
+        it = row//3
+        jt = col//3
+        for row in range(it * 3, it * 3 + 3):
+            for col in range (jt * 3, jt * 3 + 3):
+                if grid[row][col] == val:
+                    return False
+        return True
+
+
+    #adding numbers to the board until full using backtracking
+    def addNum2Board(t_grid):
+        numberList = [0,1,2,3,4,5,6,7,8,9]
+
+        for i in range(0,81):
+            row = i // 9
+            col = i % 9
+            
+            if t_grid[row][col] == 0:
+                random.shuffle(numberList)
+
+                for value in numberList:
+                    if validEntry(t_grid, row, col, value):
+                        t_grid[row][col] = value
+
+                        if row == 8 & col == 8:
+                           return True
+
+                        else:
+                            if addNum2Board(t_grid):
+                               return True
+
+                break
+        t_grid[row][col] = 0
+
+
+    #trim board to desired difficulty
+    def trimBoard(repetitions, t_grid):
+        escape = False
+
+        while not escape:
+            row = random.randint(0,8)
+            col = random.randint(0,8)
+
+            if not t_grid[row][col] == 0:
+                t_grid[row][col] = 0
+                escape = True
+
+        if repetitions == 0:
+            return t_grid
+
+        else:
+             trimBoard(repetitions - 1, t_grid)
+
+
+    #creates board using addNum2Board()
+    def createBoard(difficulty):
+        #determining how many numbers to start with on the board, based on difficulty
+        #if only python had switch statements lol
+        generate = int()
+        if (difficulty == "EASY"):
+            generate = random.randint(37,50)
+        elif (difficulty == "MED"):
+            generate = random.randint(21,30)
+        elif (difficulty == "HARD"):
+            generate = random.randint(13,18)
+        else:
+            generate = random.randint(10,50)
+
+        addNum2Board(grid)
+
+        return trimBoard(81 - generate, grid)
+    
+    #initialize the grid depending on the difficulty
+    createBoard(difficulty)
 
     #running the program
     run = True
@@ -116,7 +212,7 @@ if __name__ == "__main__":
                         print("ERROR")
 
         screen.fill((255, 255, 255))
-        draw()
+        draw(grid)
         pygame.display.update()
 
     pygame.quit()
